5a0db5f01febcf18c6b050dd2df0d90438c08a2a
khasogi27 <khasogi27@live.com>
2019-12-02 10:04:20 +0700

    baru 1 soal, 1 lgi nyusul hehe

---
+++ tugasDay6.js
@@ -0,0 +1,16 @@
+// 1. cari berapa kata huruf k, munculkan angkanya 
+
+// var abj = 'abcdefghijaosjdlkadjlaksdjlaksdjlasdj a;sjdalksdjaklsdj lasjdlkasdjklasdjaskldjalsdkjskdjkkkkkkkkklmnopqrs'
+// var num = (abj.match(/k/g) || []).length;
+// if(num == 0) {
+//     console.log('nothing k');
+// } 
+// else {
+//     console.log('Jumlah huruf k : ' + num);
+// }
+
+
+// 2.  ubah huruf S menjadi spasi
+
+// let words = 'hondaStoyotaSHyundaiSMitsubishi'
+// let output = '' // 'honda toyota Mitsubishi'
